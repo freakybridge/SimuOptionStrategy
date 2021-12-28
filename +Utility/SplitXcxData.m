684034c4b62a8c6fd82446ca7ee1aa7e3c4cb7fd
freakybridge <freakybridge@vip.qq.com>
2021-12-28 10:02:36 +0800

    优化效率

--- +Utility/SplitXcxData.m
+++ +Utility/SplitXcxData.m
@@ -1,5 +1,9 @@
 % 分解XCX OPTION DATA
 function SplitXcxData(root_, pth_hm)
+% 预处理
+file_dat_buffer = cell(0, 9);
+symb_dat_map = containers.Map;
+
 % 路径配置
 dir_raw = fullfile(root_, 'raw');
 dir_final = fullfile(root_, 'final');
@@ -8,29 +12,34 @@ dir_final = fullfile(root_, 'final');
 file_raw = dir(dir_raw);
 file_raw = file_raw(~[file_raw.isdir]);
 
-symb_dat_map = containers.Map;
-loc_col = [1, 2, 4 : 9, 12];
+% 读取所有文件
 for i = 1 : length(file_raw)
-    % 读取数据
     this_file = file_raw(i);
-    fprintf('Reading file %s, %i/%i, please wait ...\r', this_file.name, i, length(file_raw));
+    fprintf('Scanning file %s, %i/%i, please wait ...\r', this_file.name, i, length(file_raw));
     [~, ~, dat] = xlsread(fullfile(this_file.folder, this_file.name));
     dat(1, :) = [];
-    dat = dat(:, loc_col);
+    dat = dat(:, [1, 2, 4 : 9, 12]);
+    file_dat_buffer = [file_dat_buffer; dat];
+end
+
+
+% 信息按代码存入缓存
+symbols = unique(file_dat_buffer(:, 1));
+for i = 1 : length(symbols)
+    key = symbols{i};
+    fprintf('Extracting option %s data, %i/%i, please wait ...\r', key, i, length(symbols));    
     
-    % 存入缓存
-    symbols = unique(dat(:, 1));
-    for j = 1 : length(symbols)
-        key = symbols{j};
-        tmp_dat = dat(ismember(dat(:, 1), key), 2 : end);
-        tmp_dat = [datenum(tmp_dat(:, 1)), cell2mat(tmp_dat(:, 2 : end))];
-        tmp_dat = tmp_dat(:, [1 : 5, 7, 6, 8]);
-        if (symb_dat_map.isKey(key))
-            symb_dat_map(key) = [symb_dat_map(key); tmp_dat];
-        else
-            symb_dat_map(key) = tmp_dat;
-        end
-    end    
+    loc = ismember(file_dat_buffer(:, 1), key);
+    tmp_dat = file_dat_buffer(loc, 2 : end);
+    tmp_dat = [datenum(tmp_dat(:, 1)), cell2mat(tmp_dat(:, 2 : end))];
+    tmp_dat = tmp_dat(:, [1 : 5, 7, 6, 8]);
+    if (symb_dat_map.isKey(key))
+        symb_dat_map(key) = [symb_dat_map(key); tmp_dat];
+    else
+        symb_dat_map(key) = tmp_dat;
+    end
+    
+    file_dat_buffer(loc, :) = [];
 end
 
 % 读取合约信息
