c9d8338766346bb3fed4abf61277ea438ac6e76d
freakybridge <freakybridge@vip.qq.com>
2022-01-09 23:59:28 +0800

    tmp sav

--- draft/draft.m
+++ draft/draft.m
@@ -5,6 +5,6 @@ instrus = Utility.ReadSheet('E:\OneDrive\hisdata', 'instrument');
 for i = 1 : size(instrus, 1)
     info = instrus(i, :);
     opt = BaseClass.Asset.Option("5M", info{1},  info{2},  info{3},  info{7}, [[930, 1130]; [1300, 1500]], info{4}, info{5}, info{6}, info{8}, info{9});
-    fprintf("Loading %s market data, %i/%i, please wait ...\r", info{1}, 1, size(instrus, 1));
+    fprintf("Loading %s market data, %i/%i, please wait ...\r", info{1}, i, size(instrus, 1));
     dm.LoadMd(opt, 'C:\Users\freakybridge\Desktop\taobao\final', 'C:\Users\freakybridge\Desktop\taobao');
 end
