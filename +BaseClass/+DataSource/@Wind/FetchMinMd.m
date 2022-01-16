3a36890c76c6999952dc1b06d72de9e3b267b92b
freakybridge <freakybridge@vip.qq.com>
2022-01-17 00:27:55 +0800

    完成DataSource\Wind\FetchMin系列

--- +BaseClass/+DataSource/@Wind/FetchMinMd.m
+++ +BaseClass/+DataSource/@Wind/FetchMinMd.m
@@ -1,7 +1,7 @@
 % Wind 获取分钟数据
 % v1.3.0.20220113.beta
 %       1.首次加入
-function [is_err, md] = FetchMinMd(obj, asset, ts_s, ts_e, inv, err_fmt)
+function [is_err, md] = FetchMinMd(obj, asset, ts_s, ts_e, err_fmt)
 
 % 确定是否数据超限
 if (datenum(ts_s) < now - obj.FetchApiDateLimit())
@@ -10,10 +10,21 @@ if (datenum(ts_s) < now - obj.FetchApiDateLimit())
     return;
 end
 
-% 下载
+% 参数修正
 exc = obj.exchanges(EnumType.Exchange.ToString(asset.exchange));
+switch asset.interval
+    case EnumType.Interval.min1
+        inv = '1';
+    case EnumType.Interval.min5
+        inv = '5';
+    otherwise
+        error('Unexpected interval for [%s.%s] minute md fetching', asset.symbol, exc)
+end
+
+
+% 下载
 [md, ~, ~, dt, obj.err.code, ~] = obj.api.wsi([asset.symbol, '.', exc], 'open,high,low,close,amt,volume,oi', ...
-    datestr(ts_s, 'yyyy-mm-dd HH:MM:SS'), datestr(ts_e, 'yyyy-mm-dd HH:MM:SS'), sprintf('BarSize=%i',  inv));
+    datestr(ts_s, 'yyyy-mm-dd HH:MM:SS'), datestr(ts_e, 'yyyy-mm-dd HH:MM:SS'), sprintf('BarSize=%s',  inv));
 
 % 输出
 if (obj.err.code)
--- +BaseClass/+DataSource/@Wind/FetchMinMdFuture.m
+++ +BaseClass/+DataSource/@Wind/FetchMinMdFuture.m
@@ -1,6 +1,6 @@
 % Wind 获取期货分钟数据
 % v1.3.0.20220113.beta
 %       1.首次加入
-function [is_err, md] = FetchMinMdFuture(obj, fut, ts_s, ts_e, inv)
-[is_err, md] = obj.FetchMinMd(fut, ts_s, ts_e, inv, 'Fetching future %s.%s minitue market data');
+function [is_err, md] = FetchMinMdFuture(obj, fut, ts_s, ts_e)
+[is_err, md] = obj.FetchMinMd(fut, ts_s, ts_e, 'Fetching future [%s.%s] minitue market data');
 end
