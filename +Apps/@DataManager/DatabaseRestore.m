7fb222dc25117341f817ca47ff49afffd52d5718
freakybridge <freakybridge@vip.qq.com>
2022-01-24 16:07:09 +0800

    完成 DataManager->DatabaseRestore 优化 DataRecorder->LoadChainOption

--- +Apps/@DataManager/DatabaseRestore.m
+++ +Apps/@DataManager/DatabaseRestore.m
@@ -6,9 +6,30 @@ function DatabaseRestore(obj, dir_bak)
 % 预处理
 folder_ins = 'INSTRUMENTS';
 folder_cal = 'CALENDAR';
+folders = LocateAvailablePath(dir_bak, folder_ins, folder_cal);
 
+% 逐一入库
+for i = 1 : length(folders)
+    fprintf(2, 'Restoring progress [%i/%i], please wait ...\r', i ,length(folders));
+    this = folders(i);
+    switch this.name
+        case folder_cal
+            % 入库日历
+            RestoreCalendar(obj, dir_bak);
+            
+        case folder_ins
+            % 入库合约表
+            RestoreInstruments(obj, dir_bak, folder_ins);            
+            
+        otherwise
+            % 入库资产
+            RestoreAsset(obj, dir_bak, this.name);
+    end
+end
+end
 
-% 读取所有文件
+% 定位可用路径
+function folders = LocateAvailablePath(dir_bak, folder_ins, folder_cal)
 folders = dir(dir_bak);
 for i = length(folders) : -1 : 1
     this = folders(i);
@@ -19,7 +40,7 @@ for i = length(folders) : -1 : 1
     elseif (~this.isdir)
         folders(i) = [];
     elseif (strcmp(this.name, folder_ins) || (strcmp(this.name, folder_cal)))
-        continue;        
+        continue;
     else
         loc = strfind(this.name, '-');
         if (isempty(loc))
@@ -35,35 +56,8 @@ for i = length(folders) : -1 : 1
         end
     end
 end
-
-% 逐一入库
-for i = 1 : length(folders)
-    this = folders(i);
-    switch this.name
-        case folder_cal
-            % 入库日历
-            continue;
-            RestoreCalendar(obj, dir_bak);
-            
-        case folder_ins
-            % 入库合约表
-            continue;
-            RestoreInstruments(obj, dir_bak, folder_ins);
-            
-            
-        otherwise
-            
-            
-            % 生成资产
-            
-            % 读取行情
-            
-            % 入库
-
-            continue;
-    end
 end
-end
+
 
 % 入库日历
 function RestoreCalendar(obj, dir_bak)
@@ -96,6 +90,109 @@ for i = 1 : length(files)
     
     ins = obj.dr.LoadChain(product, variety, exchange, dir_bak);
     obj.db.SaveChain(product, variety, exchange, ins);
+end
+end
+
+% 入库资产
+function RestoreAsset(obj, dir_bak, folder)
+% 确定周期 / 品种
+loc = strfind(folder, '-');
+switch length(loc)
+    case 1
+        inv = EnumType.Interval.ToEnum(folder(1 : loc(1) - 1));
+        product = EnumType.Product.ToEnum(folder(loc(1) + 1 : end));
+        
+    case 3
+        inv = EnumType.Interval.ToEnum(folder(1 : loc(1) - 1));
+        product = EnumType.Product.ToEnum(folder(loc(1) + 1 : loc(2) - 1));
+        
+    otherwise
+        error('Can''t recognise folder ''%s\\'', please check !', fullfile(dir_bak, folder));
+end
+
+% 读取文件
+path = fullfile(dir_bak, folder);
+files = dir(path);
+files([files.isdir]) = [];
+
+% 逐一入库
+switch product
+    case {EnumType.Product.ETF, EnumType.Product.Index}
+        RestoreEtfIndex(obj, product, inv, dir_bak, folder, files);
+        
+    case {EnumType.Product.Future}
+        RestoreFuture(obj, inv, dir_bak, folder, files);
+        
+    case {EnumType.Product.Option}
+        RestoreOption(obj, inv, dir_bak, folder, files);
+        
+    otherwise
+        error('Unexpected condition, please check !');
+end
+end
+
+% 还原 ETF / Index
+function RestoreEtfIndex(obj, product, inv, dir_bak, folder, files)
+for i = 1 : length(files)
+    this = files(i);
+    loc1 = strfind(this.name, '_');
+    loc2 = strfind(this.name, '.');
+    variety = this.name(1 : loc1 - 1);
+    exchange = EnumType.Exchange.ToEnum(this.name(loc1 + 1 : loc2 - 1));
+    asset = BaseClass.Asset.Asset.Selector(product, variety, exchange, inv);
+    fprintf(2, 'Restoring [%s]-[%s-%s], [%i/%i], please wait ...\r', folder, asset.symbol, Utility.ToString(asset.exchange), i, length(files));
+    obj.dr.LoadMarketData(asset, dir_bak);
+    obj.db.SaveMarketData(asset);
+end
+end
+
+% 还原 Future
+function RestoreFuture(obj, inv, dir_bak, folder, files)
+% 确定品种
+loc = strfind(folder, '-');
+variety = folder(loc(2) + 1 : loc(3) - 1);
+exchange = EnumType.Exchange.ToEnum(folder(loc(3) + 1 : end));
+
+% 生成样本
+asset = BaseClass.Asset.Asset.Selector(EnumType.Product.Future, variety, exchange, 'Symbol', 'SECNAME', inv, 100, datestr(now()),  datestr(now()), 0.12, 1, 0.05);
+
+% 还原
+for i = 1 : length(files)
+    this = files(i);
+    loc = strfind(this.name, '.');
+    symb = this.name(1 : loc(1) - 1);
+    asset.symbol = symb;
+    fprintf(2, 'Restoring [%s]-[%s], [%i/%i], please wait ...\r', folder, symb, i, length(files));
+    obj.dr.LoadMarketData(asset, dir_bak);
+    obj.db.SaveMarketData(asset);
+end
+end
+
+% 还原 Option
+function RestoreOption(obj, inv, dir_bak, folder, files)
+% 确定品种
+loc = strfind(folder, '-');
+variety = folder(loc(2) + 1 : loc(3) - 1);
+exchange = EnumType.Exchange.ToEnum(folder(loc(3) + 1 : end));
+
+
+% 生成样本
+switch upper(variety)
+    case {'159919', '510050', '510300', 'IO'}
+        asset = BaseClass.Asset.Asset.Selector(EnumType.Product.Option, variety, exchange, 'Symbol', 'SECNAME', inv, 100, EnumType.CallOrPut.Call, 1000, datestr(now()),  datestr(now()));
+        
+    otherwise
+        asset = BaseClass.Asset.Asset.Selector(EnumType.Product.Option, variety, exchange, 'Symbol', 'SECNAME', inv, 100, EnumType.CallOrPut.Call, 1000, datestr(now()),  datestr(now()), ...
+            'Future Symbol', 'Future SECNAME', 100, datestr(now()),  datestr(now()), 0.12, 1, 0.05);
+end
+
+% 还原
+for i = 1 : length(files)
+    this = files(i);
+    asset.symbol = this.name(1 : strfind(this.name, '.') - 1);
+    fprintf(2, 'Restoring [%s]-[%s], [%i/%i], please wait ...\r', folder, asset.symbol, i, length(files));
+    obj.dr.LoadMarketData(asset, dir_bak);
+    obj.db.SaveMarketData(asset);
 end
 end
 
--- +Apps/@DataRecorder/LoadChainOption.m
+++ +Apps/@DataRecorder/LoadChainOption.m
@@ -25,36 +25,33 @@ instrus = UnifyInstruFmt(EnumType.Product.Option, instrus);
 end
 
 % 统一合约表格式
-function ret = UnifyInstruFmt(pdt, instrus)
+function ret = UnifyInstruFmt(pdt, ins)
 
 switch pdt
     case EnumType.Product.Option
-        ret = cell(size(instrus));
-        for i = 1 : size(instrus, 1)
-            this = instrus(i, :);
-            ret{i, 1} = Utility.ToString(this.SYMBOL);
-            ret{i, 2} = this.SEC_NAME;
-            ret{i, 3} = this.EXCHANGE{:};
-            ret{i, 4} = Utility.ToString(this.VARIETY);
-            ret{i, 5} = Utility.ToString(this.UD_SYMBOL);
-            ret{i, 6} = this.UD_PRODUCT{:};
-            ret{i, 7} = this.UD_EXCHANGE{:};
-            ret{i, 8} = this.CALL_OR_PUT{:};
-            ret{i, 9} = this.STRIKE_TYPE{:};
-            ret{i, 10} = this.STRIKE;
-            ret{i, 11} = this.SIZE;
-            ret{i, 12} = this.TICK_SIZE;
-            ret{i, 13} = this.DLMONTH;
-            ret{i, 14} = datestr(this.START_TRADE_DATE, 'yyyy-mm-dd HH:MM');
-            ret{i, 15} = datestr(this.END_TRADE_DATE, 'yyyy-mm-dd HH:MM');
-            ret{i, 16} = this.SETTLE_MODE{:};
-            ret{i, 17} = datestr(this.LAST_UPDATE_DATE, 'yyyy-mm-dd HH:MM');
-        end
-        ret = cell2table(ret, 'VariableNames', instrus.Properties.VariableNames);
+        ret = cell(size(ins));
+        ret(:, 1) = cellfun(@(x) {Utility.ToString(x)}, ins.SYMBOL);
+        ret(:, 2) = ins.SEC_NAME;
+        ret(:, 3) = ins.EXCHANGE;
+        ret(:, 4) = cellfun(@(x) {Utility.ToString(x)}, ins.VARIETY);
+        ret(:, 5) = cellfun(@(x) {Utility.ToString(x)}, ins.UD_SYMBOL);
+        ret(:, 6) = ins.UD_PRODUCT;
+        ret(:, 7) = ins.UD_EXCHANGE;
+        ret(:, 8) = ins.CALL_OR_PUT;
+        ret(:, 9) = ins.STRIKE_TYPE;
+        ret(:, 10) = num2cell(ins.STRIKE);
+        ret(:, 11) = num2cell(ins.SIZE);
+        ret(:, 12) = num2cell(ins.TICK_SIZE);
+        ret(:, 13) = num2cell(ins.DLMONTH);
+        ret(:, 14) = cellfun(@(x) {datestr(x, 'yyyy-mm-dd HH:MM')}, ins.START_TRADE_DATE);
+        ret(:, 15) = cellfun(@(x) {datestr(x, 'yyyy-mm-dd HH:MM')}, ins.END_TRADE_DATE);
+        ret(:, 16) = ins.SETTLE_MODE;
+        ret(:, 17) = cellfun(@(x) {datestr(x, 'yyyy-mm-dd HH:MM')}, ins.LAST_UPDATE_DATE);
+        ret = cell2table(ret, 'VariableNames', ins.Properties.VariableNames);
         return;
-        
+              
     otherwise
-        error("Unexpected ""product"" for transmission, pleas check.");
+        error("Unexpected ""product"" for transformation, pleas check.");
         
 end
 end
