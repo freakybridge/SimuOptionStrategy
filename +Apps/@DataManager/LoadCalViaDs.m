74d210cf88649dd80e8e5a6f223bd65c92c45d52
freakybridge <freakybridge@vip.qq.com>
2022-01-24 09:37:18 +0800

    DataManager / LoadingCalendar框架完成

--- +Apps/@DataManager/LoadCalViaDs.m
+++
@@ -1,6 +0,0 @@
-% 从数据接口获取交易日历
-% v1.3.0.20220113.beta
-%      1.首次加入
-function cal = LoadCalViaDs(obj)
-cal = obj.ds.FetchCalendar();
-end
--- +Apps/@DataManager/LoadCalendar.m
+++ +Apps/@DataManager/LoadCalendar.m
@@ -1,9 +1,9 @@
-% DataManager / LoadCalendar 多种方式获取交易日历
+% DataManager / LoadCalendar
 % v1.3.0.20220113.beta
-%      1.首次加入
+%      1.First Commit
 function cal = LoadCalendar(obj)
 
-% 读取数据库 / csv
+% local fetching
 cal = obj.db.LoadCalendar();
 if (isempty(cal))
     cal = obj.dr.LoadCalendar(obj.dir_csv);
@@ -15,7 +15,7 @@ elseif (~NeedUpdate(cal))
     return;
 end
     
-% 更新
+% fetch from ds / saving
 cal = obj.LoadCalViaDataSource();
 obj.db.SaveCalendar(cal);
 obj.dr.SaveCalendar(cal, obj.dir_csv);
@@ -23,16 +23,16 @@ obj.dr.SaveCalendar(cal, obj.dir_csv);
 end
 
 
-% 判定是否需要更新
+% determine whether need update
 function ret = NeedUpdate(cal)
-% 若当前无日历信息，必须更新
+% if isempty -> need update
 if (isempty(cal))
     ret = true;
     return;
 end
 
-% 若为距离已有日历不足365天，则必须更新
-if (cal(end, 5) - now() <= 365)
+% 20 days since last update -> need update
+if (now() - cal(end, 6) >= 20)
     ret = true;
 else
     ret = false;
