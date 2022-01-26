12e8d488b8775a7e396901a6817c947da2f34061
freakybridge <freakybridge@vip.qq.com>
2022-01-26 12:58:26 +0800

    优化Database->Mss->SaveBarMin效率

--- +BaseClass/+Database/@MSS/SaveBarMin.m
+++ +BaseClass/+Database/@MSS/SaveBarMin.m
@@ -10,20 +10,22 @@ if (~CheckTable(obj, db, tb))
     CreateTable(obj, conn, db, tb);
 end
 
-% 生成 sql
-sql = string();
-for i = 1 : size(asset.md, 1)
-    this = asset.md(i, :);
-    head = sprintf("IF EXISTS (SELECT * FROM [%s] WHERE [DATETIME] = '%s') UPDATE [%s] SET [OPEN] = %f, [HIGH] = %f, [LOW] = %f, [LAST] = %f, [TURNOVER] = %f, [VOLUME] = %f, [OI] = %f WHERE [DATETIME] = '%s'", ...
-        tb, datestr(this(1), 'yyyy-mm-dd HH:MM'), tb, this(4), this(5), this(6), this(7), this(8), this(9), this(10), datestr(this(1), 'yyyy-mm-dd HH:MM'));
-    tail = sprintf(" ELSE INSERT [%s]([DATETIME], [OPEN], [HIGH], [LOW], [LAST], [TURNOVER], [VOLUME], [OI]) VALUES ('%s', %f, %f, %f, %f, %f, %f, %f)", ...
-        tb, datestr(this(1), 'yyyy-mm-dd HH:MM'), this(4), this(5), this(6), this(7), this(8), this(9), this(10));
-    sql = sql + head + tail;
-end
+% 行情预处理
+md = asset.md;
+md = [arrayfun(@(x) {datestr(x, 'yyyy-mm-dd HH:MM:SS')}, md(:, 1)), num2cell(md(:, 4 : end))];
+tbs = repmat({tb}, size(md, 1), 1);
+md = [tbs, md(:, 1), tbs, md(:, 2 : end), md(:, 1), tbs, md]';
+
+% 准备sql
+sql = " IF EXISTS (SELECT * FROM [%s] WHERE [DATETIME] = '%s') UPDATE [%s] SET [OPEN] = %f, [HIGH] = %f, [LOW] = %f, [LAST] = %f, [TURNOVER] = %f, [VOLUME] = %f, [OI] = %f WHERE [DATETIME] = '%s' ELSE INSERT [%s]([DATETIME], [OPEN], [HIGH], [LOW], [LAST], [TURNOVER], [VOLUME], [OI]) VALUES ('%s', %f, %f, %f, %f, %f, %f, %f)";
+sql = repmat(sql, 1, size(md, 2));
+sql = [sql{:}];
+sql = sprintf(sql, md{:});
 
 % 入库
 exec(conn, sql);
 ret = true;
+
 end
 
 % 建表分钟K线数据
