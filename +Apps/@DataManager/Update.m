f332d55de6f80baf7f5b8d1190a660a28494c015
freakybridge <freakybridge@vip.qq.com>
2022-01-26 23:51:22 +0800

    tmp commit

--- +Apps/@DataManager/Update.m
+++ +Apps/@DataManager/Update.m
@@ -18,12 +18,24 @@ upd_lst(end + 1).product = EnumType.Product.Index;       upd_lst(end).variety = 
 upd_lst(end + 1).product = EnumType.Product.Index;       upd_lst(end).variety = '399006';     upd_lst(end).exchange = EnumType.Exchange.SZSE;
 for i = 1 : length(upd_lst)
     this = upd_lst(i);
+    fprintf('Updating [%s-%s-%s], [%i/%i], please wait ...\r', Utility.ToString(this.product), this.variety, Utility.ToString(this.exchange), i, length(upd_lst));
     asset = BaseClass.Asset.Asset.Selector(this.product, this.variety, this.exchange, inv);
     obj.LoadMd(asset);
 end
 
 % 更新 ETF
-
+inv = EnumType.Interval.day;
+upd_lst = struct;
+upd_lst.product = EnumType.Product.ETF;                      upd_lst.variety = '159919';             upd_lst.exchange = EnumType.Exchange.SZSE;
+upd_lst(end + 1).product = EnumType.Product.ETF;       upd_lst(end).variety = '510050';     upd_lst(end).exchange = EnumType.Exchange.SSE;
+upd_lst(end + 1).product = EnumType.Product.ETF;       upd_lst(end).variety = '510300';     upd_lst(end).exchange = EnumType.Exchange.SSE;
+for i = 1 : length(upd_lst)
+    this = upd_lst(i);
+    fprintf('Updating [%s-%s-%s], [%i/%i], please wait ...\r', Utility.ToString(this.product), this.variety, Utility.ToString(this.exchange), i, length(upd_lst));
+    asset = BaseClass.Asset.Asset.Selector(this.product, this.variety, this.exchange, inv);
+    obj.LoadMd(asset);
+end
+disp(12345);
 
 % 更新期权
 
