e106e3e503cffcadf5898d40d33b4b6e3ac191e8
freakybridge <freakybridge@vip.qq.com>
2022-02-06 23:29:01 +0800

    tmp sav

--- +Apps/@DataManager/Update.m
+++ +Apps/@DataManager/Update.m
@@ -9,7 +9,6 @@ function Update(obj)
 % UpdateOption(obj);
 
 InsertOptionMin(obj);
-
 end
 
 % Calendar
@@ -139,14 +138,159 @@ end
 function LoadMd(obj, asset)
 % 读取数据库
 md_local = obj.db.LoadMarketData(asset);
-if (~isempty(md_local))
+[mark, dt_s, dt_e] = NeedUpdate(obj, asset, md_local);
+if (~mark)
     return;
 end
 
-% 读取本地 csv
-md_local = obj.dr.LoadMarketData(asset, obj.dir_root);
-if (~isempty(md_local))
-    obj.db.SaveMarketData(asset, md_local);
+if (isempty(md_local))
     return;
 end
+
+if (now() < datenum(asset.GetDateExpire()))
+    return;
+end
+
+persistent counter;
+if (~isempty(counter))
+    counter = counter + 1;
+else
+    counter = 0;
+end
+
+
+datefmt = 'yyyy-mm-dd HH:MM';
+id = fopen('C:\Users\freakybridge\Desktop\err_record.txt', 'a+');
+fprintf(id, '[Counter:%d]-[Interval:%s]-[Symbol:%s]\t\rListed:%s\tExpire:%s\rMdStart:%s\tMdEnd:%s\rUpdStart:%s\tUpdEnd:%s\r\r', ...
+    counter, ...
+    Utility.ToString(asset.interval), asset.symbol, ...
+    datestr(asset.GetDateListed(), datefmt), ...
+    datestr(asset.GetDateExpire(), datefmt), ...
+    datestr(md_local(1, 1), datefmt), ...
+    datestr(md_local(end, 1), datefmt), ...
+    datestr(dt_s, datefmt), ...
+    datestr(dt_e, datefmt));
+fclose(id);
+
+% % 读取本地 csv
+% md_local = obj.dr.LoadMarketData(asset, obj.dir_root);
+% [mark, dt_s, dt_e] = NeedUpdate(obj, asset, md_local);
+% if (~mark)
+%     obj.db.SaveMarketData(asset, md_local);
+%     return;
+% end
+
+end
+
+% Debug Function
+function [mark, dt_s, dt_e] = NeedUpdate(obj, asset, md)
+% 读取交易日历 / 获取最后交易日
+persistent cal;
+if (isempty(cal))
+    cal = obj.LoadCalendar();
+end
+if hour(now()) >= 15
+    td = now();
+else
+    td = now() - 1;
+end
+last_trade_date = find(cal(:, 5) <= td, 1, 'last');
+last_trade_date = cal(find(cal(1 : last_trade_date, 2) == 1, 1, 'last'), 5);
+
+
+if (~isempty(md))
+    % 有行情
+    % 确定理论起点终点
+    if (asset.product == EnumType.Product.ETF)
+        dt_s_o = datenum(asset.GetDateInit()) + 40;
+        dt_e_o = last_trade_date + 15 / 24;
+    elseif (asset.product == EnumType.Product.Index)
+        dt_s_o = datenum(asset.GetDateInit());
+        dt_e_o = last_trade_date + 15 / 24;
+    elseif (asset.product == EnumType.Product.Future || asset.product == EnumType.Product.Option)
+        dt_s_o =  datenum(asset.GetDateListed());
+        dt_e_o = datenum(asset.GetDateExpire());
+        if (dt_e_o > last_trade_date)
+            dt_e_o = last_trade_date + 15 / 24;
+        end
+    else
+        error('Unexpected "product" for update start point determine, please check.');
+    end
+
+    % 定位已有起点终点
+    md_s = md(1, 1);
+    md_e = md(end, 1);
+
+    %  判定起点
+    if (md_s - dt_s_o >= 1)
+        dt_s = dt_s_o;
+    else
+        dt_s = md_e;
+    end
+
+    % 判定终点
+    if (asset.interval == EnumType.Interval.min1 || asset.interval == EnumType.Interval.min5)
+        if (dt_e_o - md_e < 15 / 60 / 24)
+            dt_e = md_e;
+        else
+            dt_e = dt_e_o;
+        end
+
+    elseif (asset.interval == EnumType.Interval.day)
+        dt_e_o = floor(dt_e_o);
+        if (dt_e_o - md_e < 1)
+            dt_e = md_e;
+        else
+            dt_e = dt_e_o;
+        end
+    else
+        error("Unexpected 'interval' for market data accomplished judgement, please check.");
+    end
+
+    % 判定是否更新
+    if (dt_s == dt_e && dt_e == md_e)
+        mark = false;
+    else
+        mark = true;
+    end
+
+else
+    % 无行情
+    % 确定更新起点
+    if (asset.product == EnumType.Product.ETF || asset.product == EnumType.Product.Index)
+        dt_s = datenum(asset.GetDateInit());
+    elseif (asset.product == EnumType.Product.Future || asset.product == EnumType.Product.Option)
+        dt_s =  datenum(asset.GetDateListed());
+    else
+        error('Unexpected "product" for update start point determine, please check.');
+    end
+
+    % 确定更新终点
+    dt_e = last_trade_date + 15 / 24;
+    mark = true;
+end
+end
+
+% Fix Local Quetos
+function FixLocalQuote()
+dir_loc
+
+% symb_mis_pre = {'10000041', '10000042', }
+% 41~48
+% 65~72
+% 615~626
+% 629~640
+% 945~946
+% 949~952
+% 1021~1022
+% 1051~1070
+
+
+% symb_mis_post
+% 933~944
+% 955, 956, 963, 964, 979, 980
+
+% symb_mis_both
+% 947, 948, 
+
 end
