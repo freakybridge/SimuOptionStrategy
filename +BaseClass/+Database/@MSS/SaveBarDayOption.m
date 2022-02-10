73e34d9b20d637ff7e868455af0bfac5f683b264
freakybridge <freakybridge@vip.qq.com>
2022-02-11 00:33:17 +0800

    debug 1d index option

--- +BaseClass/+Database/@MSS/SaveBarDayOption.m
+++ +BaseClass/+Database/@MSS/SaveBarDayOption.m
@@ -31,7 +31,7 @@ for i = 1 : length(steps)
     md_in = md(:, loc_s : loc_e);
 
     % Èë¿â
-    tmp = '("%s", %f, %f, %f, %f, %f, %f, %f, %f, %f, %i, %i),';
+    tmp = '(''%s'', %f, %f, %f, %f, %f, %f, %f, %f, %f, %i, %i),';
     tmp = repmat(tmp, 1, size(md_in, 2));
     tmp(end) = ';';
     tmp = ['INSERT INTO [%s] ([DATETIME], [OPEN], [HIGH], [LOW], [LAST], [TURNOVER], [VOLUME], [OI], [PRE_SETTLE], [SETTLE], [REM_N], [REM_T]) VALUES', tmp];
