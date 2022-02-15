0548940bee3fc19f6345e519bcd941b0836c9071
freakybridge <freakybridge@vip.qq.com>
2022-02-15 14:31:08 +0800

    tmp commit

--- draft.m
+++ draft.m
@@ -1,56 +1,23 @@
-% clear;
-% clc;
-% tic;
-% 
-% import Apps.DataManager;
-% import BaseClass.Asset.Asset;
-% import EnumType.Product;
-% import EnumType.Exchange;
-% 
-% 
-% % dir_tb = 'C:\Users\freakybridge\Desktop\taobao';
-% % dir_rt = "E:\OneDrive\hisdata";
-% % dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
-% 
-% dir_tb = 'C:\Users\dell\Desktop\taobao';
-% dir_rt = "D:\OneDrive\hisdata";
-% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
-% 
-% 
-% dm.Update();
-% % dm.DatabaseBackup('C:\Users\freakybridge\Desktop\Backup');
-% % dm.DatabaseRestore(dir_rt);
-% toc;
+clear;
+clc;
+tic;
+
+import Apps.DataManager;
+import BaseClass.Asset.Asset;
+import EnumType.Product;
+import EnumType.Exchange;
 
 
-ts = BaseClass.DataSource.Tushare('c5ccec0957ff2142dc1aaa2d6c34f6db1cf7cc41f718475266f7ad0b');
-jq = BaseClass.DataSource.JoinQuant('18162753893', '1101BXue');
+% dir_tb = 'C:\Users\freakybridge\Desktop\taobao';
+% dir_rt = "E:\OneDrive\hisdata";
+% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
+
+dir_tb = 'C:\Users\dell\Desktop\taobao';
+dir_rt = "D:\OneDrive\hisdata";
+dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
 
 
-
-
-
-pdt = EnumType.Product.Index;
-symb = '000016';
-exc = EnumType.Exchange.SSE;
-inv = EnumType.Interval.day;
-ts_s = datenum('2022-02-14 13:00') - 10;
-ts_e = '2022-02-14 14:00';
-[a, b] = jq.FetchMarketData(pdt, symb, exc, inv, ts_s, ts_e);
-[a, b] = ts.FetchMarketData(pdt, symb, exc, inv, ts_s, ts_e);
-
-
-wind = BaseClass.DataSource.Wind();
-[a, b] = wind.FetchMarketData(pdt, symb, exc, inv, ts_s, ts_e);
-
-            
-%             token = 'c5ccec0957ff2142dc1aaa2d6c34f6db1cf7cc41f718475266f7ad0b'; % replace your token here
-%             api = pro_api(token);
-%             df_basic = api.query('option_basic');
-%             disp(df_basic(1:10,:));
-%             obj.user = ur;
-%             obj.password = pwd;
-%             obj.err.code = THS_iFinDLogin(ur, pwd);
-%             if (obj.err.code == 0 || obj.err.code == -201)
-%                 fprintf('DataSource [%s] Ready.\r', obj.name, obj.user);
-%             end
+dm.Update();
+% dm.DatabaseBackup('C:\Users\freakybridge\Desktop\Backup');
+% dm.DatabaseRestore(dir_rt);
+toc;
