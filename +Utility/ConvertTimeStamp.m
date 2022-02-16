9179f5ccf330ae57de6be51e20c7e10b2282c60d
freakybridge <freakybridge@vip.qq.com>
2022-02-16 15:49:10 +0800

    SaveBarMin完成

--- +Utility/ConvertTimeStamp.m
+++ +Utility/ConvertTimeStamp.m
@@ -5,6 +5,6 @@ function [str, dm, dt, tm] = ConvertTimeStamp(dm)
 str = cellstr(datestr(dm, 'yyyy-mm-dd HH:MM'));
 tmp = datevec(dm);
 dt = tmp(:, 1) * 10000 + tmp(:, 2) * 100 + tmp(:, 3);
-tm = tmp(:, 4) * 10000 + tmp(:, 5) * 100 + tmp(:, 6);
+tm = tmp(:, 4) * 100 + tmp(:, 5);
 end
 
