acbc574b1304e1bc0f9bff9a4885bb1bb86eaa07
freakybridge <freakybridge@vip.qq.com>
2022-02-16 23:17:11 +0800

    第一次入库就绪

--- draft.m
+++ draft.m
@@ -8,16 +8,16 @@ import EnumType.Product;
 import EnumType.Exchange;
 
 
-% dir_tb = 'C:\Users\freakybridge\Desktop\taobao';
-% dir_rt = "E:\OneDrive\hisdata";
-% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
+dir_tb = 'C:\Users\freakybridge\Desktop\taobao';
+dir_rt = "E:\OneDrive\hisdata";
+dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
 
-dir_tb = 'C:\Users\dell\Desktop\taobao';
-dir_rt = "D:\OneDrive\hisdata";
-dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
+% dir_tb = 'C:\Users\dell\Desktop\taobao';
+% dir_rt = "D:\OneDrive\hisdata";
+% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
 
 
 % dm.Update();
 % dm.DatabaseBackup('C:\Users\dell\Desktop\backup');
-dm.DatabaseRestore('C:\Users\dell\Desktop\test_input');
+dm.DatabaseRestore('C:\Users\freakybridge\Desktop\test');
 toc;
