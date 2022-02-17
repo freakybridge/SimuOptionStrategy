57a4c8b802557fc08f37df6e239b9efdb13155f5
freakybridge <freakybridge@vip.qq.com>
2022-02-17 11:08:30 +0800

    删除调试代码

--- +Apps/@DataRecorder/LoadBar.m
+++ +Apps/@DataRecorder/LoadBar.m
@@ -15,20 +15,4 @@ else
     md = readtable(filename);
     md = [datenum(table2array(md(:, 1))), table2array(md(:, 2 : end))];
 end
-
-if (asset.interval == EnumType.Interval.day)
-    if (asset.product == EnumType.Product.ETF)
-        md(:, [8, 9]) = md(:, [9, 8]);
-    elseif (asset.product == EnumType.Product.Future)
-        md(:, [6, 7]) = md(:, [7, 6]);
-    elseif (asset.product == EnumType.Product.Index)
-        md(:, [6, 7]) = md(:, [7, 6]);
-    elseif (asset.product == EnumType.Product.Option)
-        md(:, [6, 7]) = md(:, [7, 6]);
-    end
-
-else
-    md(:, [6, 7]) = md(:, [7, 6]);
-end
-
 end
