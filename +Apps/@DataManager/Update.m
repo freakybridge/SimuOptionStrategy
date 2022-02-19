d24e03441735cc0c9d6c7793f86c8d29cfd65641
freakybridge <freakybridge@vip.qq.com>
2022-02-20 00:00:57 +0800

    DataManager->Update对接新版本SampleOption

--- +Apps/@DataManager/Update.m
+++ +Apps/@DataManager/Update.m
@@ -131,7 +131,7 @@ for i = 1 : length(upd_lst)
     % 载入行情摘要
     this = upd_lst(i);
     info = ins(1, :);
-    sample = SampleOption(info, this.product, this.variety, this.exchange, inv);
+    sample = Utility.SampleOption(this.product, this.variety, this.exchange, inv, info);
     views = obj.db.LoadOverviews(sample);
 
     % 更新
@@ -144,7 +144,7 @@ for i = 1 : length(upd_lst)
         view = views(ismember(views.TABLENAME, info.SYMBOL{:}), :);
         if (~isempty(view) && view.COUNTS)
             if ((inv == EnumType.Interval.day && datenum(view.TS_END) < datenum(info.END_TRADE_DATE{:}, 'yyyy-mm-dd')) || (inv == EnumType.Interval.min5 && datenum(view.TS_END) < datenum(info.END_TRADE_DATE{:}, 'yyyy-mm-dd HH:MM')))
-                asset = SampleOption(info, this.product, this.variety, this.exchange, inv);
+                asset = Utility.SampleOption(this.product, this.variety, this.exchange, inv, info);
                 [mark, ~, ~] = obj.NeedUpdate(asset, datenum(view.TS_START), datenum(view.TS_END));
                 if (~mark)
                     continue;
@@ -153,7 +153,7 @@ for i = 1 : length(upd_lst)
                 continue;
             end
         else
-            asset = SampleOption(info, this.product, this.variety, this.exchange, inv);
+            asset = Utility.SampleOption(this.product, this.variety, this.exchange, inv, info);
         end
 
         % check local csv md
@@ -176,26 +176,6 @@ end
 
 end
 
-% 获取期权样本
-function asset_ = SampleOption(info_, pdt_, var_, exc_, inv_)
-switch var_
-    case {'159919', '510050', '510300'}
-        asset_ = BaseClass.Asset.Asset.Selector(pdt_, var_, exc_, ...
-            info_.SYMBOL{:}, ...
-            info_.SEC_NAME{:}, ...
-            inv_, ...
-            info_.SIZE, ...
-            EnumType.CallOrPut.ToEnum(info_.CALL_OR_PUT{:}), ...
-            info_.STRIKE, ...
-            info_.START_TRADE_DATE{:}, ...
-            info_.END_TRADE_DATE{:}...
-            );
-
-    otherwise
-        error('Unsupported variety for updating, please check !');
-end
-end
-
 %% DEBUG
 % % Debug Function：Check Option Min Md Data
 % function InsertOptionMin(obj)
@@ -207,7 +187,7 @@ end
 %     this = upd_lst(i);
 %     fprintf('Updating [%s-%s-%s], [%i/%i], please wait ...\r', Utility.ToString(this.product), this.variety, Utility.ToString(this.exchange), i, length(upd_lst));
 %     ins = obj.LoadChain(this.product, this.variety, this.exchange);
-% 
+%
 %     % 更新
 %     for j = 1 : height(ins)
 %         info = ins(j, :);
@@ -223,22 +203,22 @@ end
 %                     info.START_TRADE_DATE{:}, ...
 %                     info.END_TRADE_DATE{:}...
 %                     );
-% 
+%
 %             otherwise
 %                 error('Unsupported variety for updating, please check !');
 %         end
 %         fprintf('Updating [%s-%s-%s-%s], [%i/%i], please wait ...\r', Utility.ToString(this.product), this.variety, Utility.ToString(this.exchange), asset.symbol, j, height(ins));
-% 
+%
 %         asset.interval = EnumType.Interval.day;
 %         LoadMd(obj, asset);
 %         asset.md = [];
-% 
+%
 %         asset.interval = EnumType.Interval.min5;
 %         LoadMd(obj, asset);
 %     end
 % end
 % end
-% 
+%
 % % Debug Function: Load Data without datasource
 % function LoadMd(obj, asset)
 % % 读取数据库
@@ -247,7 +227,7 @@ end
 % if (~mark)
 %     return;
 % end
-% 
+%
 % % 读取本地 csv
 % md_local = obj.dr.LoadMarketData(asset, obj.dir_root);
 % [mark, dt_s, dt_e] = NeedUpdate(obj, asset, md_local);
@@ -256,22 +236,22 @@ end
 %     obj.db.SaveMarketData(asset, md_local);
 %     return;
 % end
-% 
+%
 % if (isempty(md_local))
 %     return;
 % end
-% 
+%
 % if (now() < datenum(asset.GetDateExpire()))
 %     return;
 % end
-% 
+%
 % persistent counter;
 % if (~isempty(counter))
 %     counter = counter + 1;
 % else
 %     counter = 0;
 % end
-% 
+%
 % datefmt = 'yyyy-mm-dd HH:MM';
 % id = fopen('C:\Users\dell\Desktop\err_record.txt', 'a+');
 % fprintf(id, '[Counter:%d]-[Interval:%s]-[Symbol:%s]\t\rListed:%s\tExpire:%s\rMdStart:%s\tMdEnd:%s\rUpdStart:%s\tUpdEnd:%s\r\r', ...
@@ -285,7 +265,7 @@ end
 %     datestr(dt_e, datefmt));
 % fclose(id);
 % end
-% 
+%
 % % Debug Function: Judge whether need update
 % function [mark, dt_s, dt_e] = NeedUpdate(obj, asset, md)
 % % 读取交易日历 / 获取最后交易日
@@ -300,8 +280,8 @@ end
 % end
 % last_trade_date = find(cal(:, 5) <= td, 1, 'last');
 % last_trade_date = cal(find(cal(1 : last_trade_date, 2) == 1, 1, 'last'), 5);
-% 
-% 
+%
+%
 % if (~isempty(md))
 %     % 有行情
 %     % 确定理论起点终点
@@ -320,18 +300,18 @@ end
 %     else
 %         error('Unexpected "product" for update start point determine, please check.');
 %     end
-% 
+%
 %     % 定位已有起点终点
 %     md_s = md(1, 1);
 %     md_e = md(end, 1);
-% 
+%
 %     %  判定起点
 %     if (md_s - dt_s_o >= 1)
 %         dt_s = dt_s_o;
 %     else
 %         dt_s = md_e;
 %     end
-% 
+%
 %     % 判定终点
 %     if (asset.interval == EnumType.Interval.min1 || asset.interval == EnumType.Interval.min5)
 %         if (dt_e_o - md_e < 15 / 60 / 24)
@@ -339,7 +319,7 @@ end
 %         else
 %             dt_e = dt_e_o;
 %         end
-% 
+%
 %     elseif (asset.interval == EnumType.Interval.day)
 %         dt_e_o = floor(dt_e_o);
 %         if (dt_e_o - md_e < 1)
@@ -350,14 +330,14 @@ end
 %     else
 %         error("Unexpected 'interval' for market data accomplished judgement, please check.");
 %     end
-% 
+%
 %     % 判定是否更新
 %     if (dt_s == dt_e && dt_e == md_e)
 %         mark = false;
 %     else
 %         mark = true;
 %     end
-% 
+%
 % else
 %     % 无行情
 %     % 确定更新起点
@@ -368,20 +348,20 @@ end
 %     else
 %         error('Unexpected "product" for update start point determine, please check.');
 %     end
-% 
+%
 %     % 确定更新终点
 %     dt_e = last_trade_date + 15 / 24;
 %     mark = true;
 % end
 % end
-% 
-% 
+%
+%
 %% Fix Local Quetos
 % function FixLocalQuote(obj)
 % %% preprocess
 % dir_udt = 'D:\OneDrive\hisdata\update';
 % dir_out = 'D:\OneDrive\hisdata\update\fixed';
-% 
+%
 % % symb_mis_front
 % % 41~48             8
 % % 65~72             8
@@ -400,25 +380,25 @@ end
 % tmp = [tmp; (10001021 : 10001022)'];
 % tmp = [tmp; (10001151 : 10001170)'];
 % symb_mis_front = arrayfun(@(x) {num2str(x)}, tmp);
-% 
+%
 % % symb_mis_rear
 % % 933~944       12
 % % 955, 956, 963, 964, 979, 980      6
 % tmp = (10000933 : 10000944)';
 % tmp = [tmp; [10000955, 10000956, 10000963, 10000964, 10000979, 10000980]'];
 % symb_mis_rear = arrayfun(@(x) {num2str(x)}, tmp);
-% 
+%
 % % symb_mis_both
 % % 947, 948,         2
 % tmp = (10000947 : 10000948)';
 % symb_mis_both = arrayfun(@(x) {num2str(x)}, tmp);
-% 
+%
 % % load instrus & calendar & symbol pool
 % ins = obj.LoadChain(EnumType.Product.Option, '510050', EnumType.Exchange.SSE);
 % cal = LoadCalendar(obj);
 % symbols = union(union(symb_mis_front, symb_mis_rear), symb_mis_both);
-% 
-% 
+%
+%
 % %% fix operation
 % for i = 1 : length(symbols)
 %     % fetch local queto
@@ -435,10 +415,10 @@ end
 %         info.END_TRADE_DATE{:}...
 %         );
 %     LoadMd(obj, asset);
-% 
+%
 %     % fetch update queto
 %     md_upd = LoadUpdateMd(dir_udt, symb{:});
-% 
+%
 %     if (ismember(symb, symb_mis_front))
 %         % missing front
 %         dt_fix_s = str2double(datestr(asset.GetDateListed(), 'yyyymmdd'));
@@ -447,7 +427,7 @@ end
 %         fprintf(2, '%s missing [front], %i day(s), total progress %i/%i,, please wait ...\r', symb{:}, length(dt_missing), i, length(symbols));
 %         disp(datestr(dt_missing));
 %         fprintf('\r');
-% 
+%
 %     elseif (ismember(symb, symb_mis_rear))
 %         % missing rear
 %         dt_fix_s = asset.md(end, 2);
@@ -456,13 +436,13 @@ end
 %         fprintf(2, '%s missing [rear], %i day(s), total progress %i/%i,, please wait ...\r', symb{:}, length(dt_missing), i, length(symbols));
 %         disp(datestr(dt_missing));
 %         fprintf('\r');
-% 
+%
 %     elseif (ismember(symb, symb_mis_both))
 %         % missing both
 %         dt_fix_s = str2double(datestr(asset.GetDateListed(), 'yyyymmdd'));
 %         dt_fix_e = asset.md(1, 2);
 %         mis_1 = cal(cal(:, 1) >= dt_fix_s & cal(:, 1) < dt_fix_e & cal(:, 2), 5);
-% 
+%
 %         dt_fix_s = asset.md(end, 2);
 %         dt_fix_e = str2double(datestr(asset.GetDateExpire(), 'yyyymmdd'));
 %         mis_2 = cal(cal(:, 1) > dt_fix_s & cal(:, 1) <= dt_fix_e & cal(:, 2), 5);
@@ -470,36 +450,36 @@ end
 %         fprintf(2, '%s missing [both], %i day(s), total progress %i/%i,, please wait ...\r', symb{:}, length(dt_missing), i, length(symbols));
 %         disp(datestr(dt_missing));
 %         fprintf('\r');
-% 
+%
 %     end
-% 
+%
 %     % fix
 %     for j = 1 : size(dt_missing, 1)
 %         md_fix = FixMarketData(dt_missing(j), md_upd);
 %         asset.MergeMarketData(md_fix);
 %     end
-% 
+%
 %     % save data
 %     SaveBar(asset, dir_out);
-% 
+%
 % end
 % end
-% 
+%
 % % debug function ： gen sse time axis
 % function axis = GenSseTimeAxis(dt_dm)
 % am = [930, 935, 940, 945, 950, 955, 1000, 1005, 1010, 1015, 1020, 1025, 1030, 1035, 1040, 1045, 1050, 1055, 1100, 1105, 1110 , 1115 , 1120 , 1125, 1130];
 % pm = [1305, 1310, 1315, 1320, 1325, 1330, 1335, 1340, 1345, 1350, 1355, 1400, 1405, 1410, 1415, 1420, 1425, 1430, 1435, 1440, 1445, 1450, 1455, 1500];
 % axis = [am, pm]';
-% 
+%
 % date = datevec(dt_dm);
 % date = repmat(date, length(axis), 1);
 % hour = floor(axis / 100);
 % date(:, 4) = hour;
 % date(:, 5) = axis - hour * 100;
-% 
+%
 % axis = datenum(date);
 % end
-% 
+%
 % % debug function ： load update maketdata
 % function md = LoadUpdateMd(dir_upd, symb)
 % md = readtable(fullfile(dir_upd, [symb, '.xlsx']), 'PreserveVariableNames', 1);
@@ -542,21 +522,21 @@ end
 %     end
 % end
 % end
-% 
+%
 % % debug func: save bar
 % function SaveBar(asset, dir_)
 % 预处理
 % 生成输出目录
 % dir_ = fullfile(dir_, BaseClass.Database.Database.GetDbName(asset));
 % Utility.CheckDirectory(dir_);
-% 
+%
 % % 生成输出文件名
 % filename = fullfile(dir_, BaseClass.Database.Database.GetTableName(asset) +  ".csv");
-% 
+%
 % % 整列表头 / 数据格式
 % header = 'datetime,open,high,low,last,turnover,volume,oi\r';
 % dat_fmt = '%s,%.4f,%.4f,%.4f,%.4f,%i,%i,%i\r';
-% 
+%
 % % 写入表头 / 写入数据
 % id = fopen(filename, 'w');
 % fprintf(id, header);
@@ -582,7 +562,7 @@ for i = 1 : length(upd_lst)
     % 载入行情摘要
     this = upd_lst(i);
     info = ins(1, :);
-    sample = SampleOption(info, this.product, this.variety, this.exchange, inv);
+    sample = Utility.SampleOption(this.product, this.variety, this.exchange, inv, info);
     views = obj.db.LoadOverviews(sample);
 
     % 更新
