4bad2dffb0cca3ffcd0bc1a970f482f8dacdae62
freakybridge <freakybridge@vip.qq.com>
2022-02-23 23:01:12 +0800

    tmp save

--- +BaseClass/+Database/@MSS/FetchAllDbs.m
+++ +BaseClass/+Database/@MSS/FetchAllDbs.m
@@ -7,18 +7,6 @@ function ret = FetchAllDbs(obj)
 db = obj.db_default;
 conn = SelectConn(obj, db);
 
-% 确定前缀
-prefix = cell(length(varargin), 1);
-for i = 1 : length(varargin)
-    prefix{i} = [varargin{i}, '%'];
-end
-
-% 生成语句
-sql = repmat(' name LIKE ''%s'' OR', 1,  length(varargin));
-sql(end - 1 : end) = [];
-sql = sprintf('DECLARE dbs CURSOR FAST_FORWARD FOR SELECT name FROM sysdatabases WHERE %s;', sql);
-sql = sprintf(sql, prefix{:});
-
 % 载入
 try
     sql = 'SELECT NAME FROM MASTER.DBO.SYSDATABASES ORDER BY NAME';
--- draft.m
+++ draft.m
@@ -6,28 +6,30 @@ import Apps.DataManager;
 import BaseClass.Asset.Asset;
 
 
-% dir_rt = "E:\OneDrive\hisdata";
-% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
+dir_rt = "E:\OneDrive\hisdata";
+dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeisbest');
 
-dir_rt = "D:\OneDrive\hisdata";
-dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
+% dir_rt = "D:\OneDrive\hisdata";
+% dm = DataManager(dir_rt, 'mss', 'sa', 'bridgeinmfc');
 
 
 % dm.Update();
 % dm.DatabaseBackup('C:\Users\freakybridge\Desktop\test');
-% dm.DatabaseRestore('D:\OneDrive\hisdata\test');
+dm.DatabaseRestore(dir_rt);
 toc;
 
 % dm.PurgeDatabase('1D', '5MIN');
 
 % db_target = {'Fund', 'Future', 'Index', 'Option'};
-% dm.DatabaseBackupOldVer('C:\Users\freakybridge\Desktop\test', db_target)
-
-ins = dm.LoadChain(EnumType.Product.Option, '510050', EnumType.Exchange.SSE);
-er = Apps.DataRecorder();
-for i = 1 : height(ins)
-    fprintf('Transferring [%s], %i/%i, please wait ...\r', ins(i, 'SYMBOL').SYMBOL{:}, i, height(ins));
-    opt = BaseClass.Asset.Option.Option.Sample('510050', EnumType.Exchange.SSE, EnumType.Interval.min5, ins(i, :));
-    dm.LoadMdViaTaobaoExcel(opt, 'D:\OneDrive\hisdata\taobao\50ETF期权5分钟数据', 'D:\OneDrive\hisdata\taobao\alter');
-    er.SaveMarketData(opt, 'C:\Users\dell\Desktop\tbtransfered');
-end
+% dm.DatabaseBackupOldVer(dir_rt, db_target)
+% 
+% ins = dm.LoadChain(EnumType.Product.Option, '510050', EnumType.Exchange.SSE);
+% er = Apps.DataRecorder();
+% for i = 1 : height(ins)
+%     fprintf('Transferring [%s], %i/%i, please wait ...\r', ins(i, 'SYMBOL').SYMBOL{:}, i, height(ins));
+%     opt = BaseClass.Asset.Option.Option.Sample('510050', EnumType.Exchange.SSE, EnumType.Interval.min5, ins(i, :));
+%     dm.LoadMdViaTaobaoExcel(opt, 'E:\OneDrive\hisdata\taobao\50ETF期权5分钟数据', 'E:\OneDrive\hisdata\taobao\alter');
+%     if (~isempty(opt.md))
+%         er.SaveMarketData(opt, dir_rt);
+%     end
+% end
